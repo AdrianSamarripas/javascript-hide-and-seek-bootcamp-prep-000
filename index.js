923d04984c7cab4561582473fe31c62413cf3a9f
Adrian Samarripas <asamarripas@gmail.com>
2018-05-05 04:08:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ nestedTarget(); {
   return document.querySelector('#nested').querySelector('.Target');
 }
 
-increaseRankBy(n){
+increaseRankBy(n); {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (i + n).toString();
+  }
 }
 
 deepestChild(){

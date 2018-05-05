25e463a193ddc6d8f8ec474d4d1bdd38b3e164fe
Adrian Samarripas <asamarripas@gmail.com>
2018-05-05 04:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ increaseRankBy(n); {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   
   for (let i = 0; i < lis.length; i++){
-    lis[i].innerHTML = (i + n).toString();
+    lis[i].innerHTML = ((parseInt(lis[i].innerHTML))+n);
   }
 }
 
